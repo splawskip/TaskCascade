47d8f10466bbb2b7a6b9bded82b29b406148a99f
Przemysław Spławski <psplawski.dev@gmail.com>
2023-02-24 17:40:31 +0100

    Include formatters and modify project configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'import/prefer-default-export': ['off'],
+  },
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+// B
 module.exports = {
-	content: [
-	  './index.html',
-	],
-	theme: {
-	  extend: {},
-	},
-	plugins: [],
-  }
+  content: ['./js/*.js', './index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        ebony: '#25273D',
+      },
+      fontFamily: {
+        sans: ['Josefin Sans', ...defaultTheme.fontFamily.sans],
+      },
+      minHeight: {
+        '1/3': 'calc(100vh/3)',
+      },
+      backgroundImage: {
+        hero: "url('/img/hero.jpg')",
+        'hero-dark': "url('/img/hero-dark.jpg')",
+      },
+      gridTemplateColumns: {
+        'two-columns-min-content': 'repeat(2, min-content)',
+      },
+    },
+  },
+  plugins: [],
+};
